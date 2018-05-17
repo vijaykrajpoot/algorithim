c2012875389578a379babc8df39540aaed43849c
Vijay <vijay.kumar@letmecall.com>
2018-05-16 18:50:57 -0700

    Init Comit

---
+++ src/main/java/com/test/algo/ReverseAnArray.java
@@ -0,0 +1,18 @@
+package com.test.algo;
+
+public class ReverseAnArray {
+
+	public static int[] reverseArray(int arr[]) {
+		
+		if (arr==null){
+			throw new IllegalArgumentException("Emopty or Null Array");
+		}
+		
+		for (int index = 0; index <= arr.length / 2 - 1; index++) {
+			int temp = arr[index];
+			arr[index] = arr[arr.length - index - 1];
+			arr[arr.length - index - 1] = temp;
+		}
+		return arr;
+	}
+}
---
+++ src/test/java/com/test/algo/ReverseAnArrayTest.java
@@ -0,0 +1,55 @@
+package com.test.algo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseAnArrayTest {
+
+	@Test
+	public void testReverseArray() {
+		int [] testArray={10,20,30,40,50};
+		int [] excepted={50,40,30,20,10};
+		int []reversedArray=ReverseAnArray.reverseArray(testArray);		
+		Assert.assertArrayEquals(excepted, reversedArray);
+	}
+	
+	@Test
+	public void testReverseArrayWhenOnly1Item() {
+		int [] testArray={10};
+		int [] excepted={10};
+		int []reversedArray=ReverseAnArray.reverseArray(testArray);		
+		Assert.assertArrayEquals(excepted, reversedArray);
+	}
+	
+	@Test
+	public void testReverseArrayWhenOnly2Item() {
+		int [] testArray={10,20};
+		int [] excepted={20,10};
+		int []reversedArray=ReverseAnArray.reverseArray(testArray);		
+		Assert.assertArrayEquals(excepted, reversedArray);
+	}
+	
+	@Test
+	public void testReverseArrayWhenNoItem() {
+		int [] testArray={};
+		int [] excepted={};
+		int []reversedArray=ReverseAnArray.reverseArray(testArray);		
+		Assert.assertArrayEquals(excepted, reversedArray);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)	
+	public void testReverseArrayWhenNullArray() {
+		int [] testArray=null;
+		int [] excepted={};
+		int []reversedArray=ReverseAnArray.reverseArray(testArray);		
+		Assert.assertArrayEquals(excepted, reversedArray);
+	}
+	
+	@Test	
+	public void testReverseArrayWhenItemsAreLessThenZERO() {
+		int [] testArray={-100,45,-10,-1000};
+		int [] excepted={-1000,-10,45,-100};
+		int []reversedArray=ReverseAnArray.reverseArray(testArray);		
+		Assert.assertArrayEquals(excepted, reversedArray);
+	}
+}
