f5cc7db34925b69794954842adedce2a8f60ac24
Vijay Kumar Rajput <vijay.x.kumar@macys.com>
2019-02-04 19:04:07 -0800

    Added Comments and Logic2 to find mid

--- src/main/java/com/code/linkedlist/algo/LinkedList.java
+++ src/main/java/com/code/linkedlist/algo/LinkedList.java
@@ -1,11 +1,23 @@
 package com.code.linkedlist.algo;
 
+/**
+ * Represent Single List List and it's operation
+ * 
+ * @author Vijay Kumar Rajput
+ *
+ */
 public class LinkedList {
 
-	Node start;
-	Node current;
+	private Node start;
+	private Node current;
 
-	void add(int data) {
+	/**
+	 * Add Operation and current pointer always move to newly added node. No need to
+	 * traverse whole list to add a new node
+	 * 
+	 * @param data
+	 */
+	public void add(int data) {
 		Node node = new Node(data);
 
 		if (start == null) {
@@ -18,6 +30,9 @@ public class LinkedList {
 
 	}
 
+	/**
+	 * Show the list
+	 */
 	void printList() {
 		Node curret = start;
 		while (curret != null) {
@@ -27,24 +42,14 @@ public class LinkedList {
 		System.out.println();
 	}
 
-	public static void main(String[] args) {
-		LinkedList linkedList = new LinkedList();
-		linkedList.add(10);
-		linkedList.add(20);
-		linkedList.add(30);
-		linkedList.add(40);
-		linkedList.add(50);
-		linkedList.add(60);
-		linkedList.add(70);
-		linkedList.add(80);
-		linkedList.add(90);
-		linkedList.add(100);
-		linkedList.add(200);
-		Node node = linkedList.findMidLogic2(linkedList.start);
-		System.out.println("Middle Node:" + node.data);
-	}
+	/**
+	 * Find Middle node Logic#1 1) Every time move node1 and move node based on
+	 * even/odd
+	 * 
+	 * @param Node:start
+	 */
 
-	void findMiddleNode(Node start) {
+	public Node findMiddleNodeLogic1(Node start) {
 		Node node1 = start;
 		Node node2 = start;
 		int i = 0;
@@ -53,10 +58,19 @@ public class LinkedList {
 			if (((i++) % 2) == 0)
 				node2 = node2.next;
 		}
-		System.out.println(node2.data);
+		return node2;
 	}
-
-	public Node findMidLogic2(Node start) {
+	
+	/**
+	 * 
+	 * Maintain 2 pointer and move pointer p1 based based on skip=true and move both pointer based on skip=false;
+	 * 
+	 * Each time pointer p1 move and p2 remains behind
+	 *  
+	 * @param start
+	 * @return
+	 */
+	public Node findMiddleNodeLogic2(Node start) {
 
 		Node p1 = start, p2 = start;
 		boolean skip = true;
@@ -70,7 +84,6 @@ public class LinkedList {
 				skip = !skip;
 			}
 		}
-
 		return p2;
 
 	}
