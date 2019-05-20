72a935eb0d529f8341ed6fed7b74735141da1be1
Vijay Kumar Rajput <vijay.x.kumar@macys.com>
2019-05-19 22:44:08 -0700

    Fixed for first Item

--- src/main/java/com/code/string/arrays/algo/BinarySearch.java
+++ src/main/java/com/code/string/arrays/algo/BinarySearch.java
@@ -3,10 +3,10 @@ package com.code.string.arrays.algo;
 public class BinarySearch {
 
 	public static boolean search(int arr[], int num) {
-		if(arr==null || arr.length==0) {
+		if (arr == null || arr.length == 0) {
 			throw new IllegalArgumentException("Input is Empty || Null");
 		}
-		return s(arr, num, 0, arr.length) > 0;
+		return s(arr, num, 0, arr.length) > -1;
 	}
 
 	private static int s(int arr[], int num, int start, int length) {
@@ -18,11 +18,13 @@ public class BinarySearch {
 
 		if (arr[mid] == num)
 			return mid;
+
 		if (arr[mid] > num) {
 			idx = s(arr, num, 0, mid);
 		} else {
 			idx = s(arr, num, mid + 1, length);
 		}
+
 		return idx;
 	}
 
