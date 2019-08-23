7a2aa1588326dfff5d82c30d7a183e1703addaae
Vijay Kumar Rajput <vijay.x.kumar@macys.com>
2019-08-23 12:35:09 -0700

    ReverseWordsInAString: Reverse Back to old logic to avoid extra space

--- src/main/java/com/algo/string/arrays/ReverseWordsInAString.java
+++ src/main/java/com/algo/string/arrays/ReverseWordsInAString.java
@@ -1,5 +1,7 @@
 package com.algo.string.arrays;
 
+import java.util.Arrays;
+
 /**
  * 
  * Write a function that reverses the order of the words in a string. For example, your function
@@ -13,8 +15,8 @@ public class ReverseWordsInAString {
 
   public static void main(String[] args) {
     String st = "Do or do not, there is no try.";
-    String rString = reverseAString(st);
     System.out.println("oString.length():" + st.length());
+    String rString = reverseAString(st);
     System.out.println("rString.length():" + rString.length());;
     System.out.println("oString:" + st);
     System.out.println("rString:" + rString);
@@ -22,36 +24,35 @@ public class ReverseWordsInAString {
 
   public static String reverseAString(String str) {
     String resversedString = reverseMe(str);
-
+    System.out.println("resversedString:" + resversedString);
     int stIndex = 0;
+    int endIndex = 0;
     String rrString = "";
-    int i = 0;
-    for (i = 0; i < resversedString.length(); i++) {
-      char ch = resversedString.charAt(i);
-      System.out.println("ch:" + ch);
+    for (char ch : resversedString.toCharArray()) {
+      endIndex++;
       if (ch == ' ') {
-        rrString = rrString + reverseMe(resversedString.substring(stIndex, i)) + ch;
-        stIndex = i;
+        rrString = rrString + reverseMe(resversedString.substring(stIndex, endIndex - 1)) + ch;
+        stIndex = endIndex;
       }
     }
-    // Exit from loop after i == string length and reverse last string
-    if (i == resversedString.length()) {
-      rrString = rrString + reverseMe(resversedString.substring(stIndex, str.length()));;
+
+    // Exit from loop after endIndex == string length and reverse last string
+    if (endIndex >= resversedString.length()) {
+      rrString = rrString + reverseMe(resversedString.substring(stIndex, resversedString.length()));;
     }
-
-
-
     return rrString;
 
   }
 
   private static String reverseMe(String str) {
     char[] ch = str.toCharArray();
+
     for (int i = 0; i < str.length() / 2; i++) {
       char temp = ch[i];
       ch[i] = ch[str.length() - 1 - i];
       ch[str.length() - 1 - i] = temp;
     }
+    System.out.println("str:" + str + ":ch.len:" + ch.length + "  :String(ch):" + new String(ch));
     return new String(ch);
   }
 
