155e14be3010242d74719fd30c88c70a88b2f205
Vijay Kumar Rajput <vijay.x.kumar@macys.com>
2019-08-30 00:36:09 -0700

    Given a list of non negative integers, arrange them such that they form the largest number.

---
+++ src/main/java/com/algo/string/arrays/LargestNumber.java
@@ -0,0 +1,52 @@
+package com.algo.string.arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 
+ * Given a list of non negative integers, arrange them such that they form the largest number.
+ * 
+ * @author Vijay Kumar Rajput
+ *
+ */
+public class LargestNumber {
+
+
+  public static void main(String[] args) {
+    int arr[] = {3, 59,30, 34,95, 5, 9};
+    System.out.println("LargestNumber.formLargestNumber(arr):"+LargestNumber.formLargestNumber(arr));
+
+  }
+
+  public static String formLargestNumber(int arr[]) {
+
+    String[] strArr = new String[arr.length];
+
+    for (int i = 0; i < arr.length; i++) {
+      strArr[i] = String.valueOf(arr[i]);
+    }
+
+    System.out.println("Before Sorting: " + Arrays.toString(strArr));
+    // Sort higer number like 95
+    Arrays.sort(strArr, new Comparator<String>() {
+      public int compare(String o1, String o2) {
+        System.err.println((o2 + o1) + "-" + (o1 + o2) + "==>" + (o2 + o1).compareTo(o1 + o2));
+        return (o2 + o1).compareTo(o1 + o2);
+      };
+    });
+
+    StringBuilder builder = new StringBuilder();
+    System.out.println("After Sorting: " + Arrays.toString(strArr));
+    for (String st : strArr) {
+      builder.append(st);
+    }
+
+    System.out.println(builder.toString());
+    return builder.toString();
+
+  }
+
+
+}
+
