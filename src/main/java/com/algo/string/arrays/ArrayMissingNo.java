e814f843baff75737726a248dd7aa1c355751e9d
Vijay Kumar Rajput <vijay.x.kumar@macys.com>
2019-09-01 23:30:10 -0700

    ArrayMissingNo.java

--- src/main/java/com/algo/string/arrays/ArrayMissingNo.java
+++ src/main/java/com/algo/string/arrays/ArrayMissingNo.java
@@ -1,18 +1,39 @@
 package com.algo.string.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayMissingNo {
   public static void main(String[] args) {
-    int[] arr = {1, 2, 3, 4, 5, 7, 6, 8, 10};
-    int a = ArrayMissingNo.getMissingNo(arr);
-    if (a < 0) {
+    int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
+    List<Integer> ll = ArrayMissingNo.getMissingNo(arr);
+    if (ll.size() <= 0) {
       System.out.println("No Missing");
     } else {
-      System.out.println("Missing No:" + a);
+      System.out.println("Missing Nos:" + Arrays.toString(ll.toArray()));
     }
   }
 
-  public static int getMissingNo(int arr[]) {
-    // TODO
-    return -1;
+  public static List<Integer> getMissingNo(int arr[]) {
+    System.out.println("arr.length:"+arr.length);
+    List<Integer> retList = new ArrayList<Integer>(arr.length);
+
+    for (int i = 0; i < arr.length; i++) {
+      int index = Math.abs(arr[i]) - 1;
+      System.out.println("Index:" + index);
+      if (arr[index] > 0) {
+        arr[index] = -arr[index];
+      }
+    }
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        retList.add(i + 1);
+      }
+    }
+
+    return retList;
+
   }
 }
