a400bbdf25a347e2ce15ff22b7abe4abb4a7ccdf
Vijay Kumar Rajput <vijay.x.kumar@macys.com>
2019-09-02 12:18:07 -0700

    Math:Prime

--- src/main/java/com/algo/PrimeNumber.java
+++ src/main/java/com/algo/PrimeNumber.java
@@ -1,12 +1,51 @@
 package com.algo;
 
+/**
+ * 
+ * Find a given number is Prime or not.
+ * 
+ * @author Vijay Kumar Rajput
+ *
+ */
 public class PrimeNumber {
   
-  
-  boolean isPrime(int numnber) {
-    
-    for(int i=0)
-    
+
+  public static void main(String[] args) {
+    int number = 3;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+    number = 7;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+    number = 25;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+    number = 343;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+    number = 0;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+    number = 2;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+    number = 1;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+
+    number = -23;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+
+
+    number = 29;
+    System.out.println("PrimeNumber.isPrime(" + number + ") :" + PrimeNumber.isPrime(number));
+
+  }
+
+  static boolean isPrime(int numnber) {
+    if (numnber <= 1) {
+      return false;
+    }
+
+    for (int i = 2; i < numnber / 2; i++) {
+      if (numnber % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
 
 }
