d40f19dc85fe1dc7ed9978c35fd8add3e3038964
Vijay Kumar Rajput <vijaykrajpoot@gmail.com>
2021-04-28 22:43:27 -0700

    TwoSum

---
+++ src/main/java/parctice/TwoSum.java
@@ -0,0 +1,22 @@
+package parctice;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int[] result = twoSum(arr, 13);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static int[] twoSum(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
