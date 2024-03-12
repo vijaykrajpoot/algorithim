7e776dc8defc751ccb1ab6163ae09955da5a5fbf
Vijay Kumar Rajput <vijay.kumar1@sony.com>
2024-03-12 13:45:06 -0700

    Tree

--- src/main/java/com/algo/tree/graph/BTreeRightSideView.java
+++ src/main/java/com/algo/tree/graph/BTreeRightSideView.java
@@ -1,24 +1,19 @@
 package com.algo.tree.graph;
 
-
-import apple.laf.JRSUIUtils;
-
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-//https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
-
-public class LevelOrderBottom {
-
+public class BTreeRightSideView {
 	public List<Integer> rightSideView(TreeNode root) {
 		Queue<TreeNode> q = new LinkedList<>();
 		List<Integer> result = new ArrayList<>();
 		q.add(root);
 		while (!q.isEmpty()) {
 			int qSize=q.size();
+			TreeNode node = q.poll();
 			TreeNode lastElement = null;
 			for (TreeNode treeNode : q) {
 				lastElement = treeNode;
@@ -26,10 +21,8 @@ public class LevelOrderBottom {
 			if (lastElement != null) {
 				result.add(lastElement.val);
 			}
+
 			for (int i = 0; i < qSize; i++) {
-				TreeNode node=q.poll();
-				if(node==null)
-					continue;
 				if (node.left != null) {
 					q.add(node.left);
 				}
